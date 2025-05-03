980923446dfe21cacd06b94cb79246d787f9482f
Agus <agusptr44@gmail.com>
2025-05-03 10:58:40 +0700

    update modul 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:praktikum_flutter1/modul_7.1.dart';
-import 'package:praktikum_flutter1/modul_7.10.dart';
-import 'package:praktikum_flutter1/modul_7.11.dart';
-import 'package:praktikum_flutter1/modul_7.2.dart';
-import 'package:praktikum_flutter1/modul_7.3.dart';
-import 'package:praktikum_flutter1/modul_7.4.dart';
-import 'package:praktikum_flutter1/modul_7.5.dart';
-import 'package:praktikum_flutter1/modul_7.6.dart';
-import 'package:praktikum_flutter1/modul_7.7.dart';
-import 'package:praktikum_flutter1/modul_7.8.dart';
-import 'package:praktikum_flutter1/modul_7.9.dart';
-import 'package:praktikum_flutter1/modul_7_evaluasi.dart';
-import 'package:praktikum_flutter1/my_home_page.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.1.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.10.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.11.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.2.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.3.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.4.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.5.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.6.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.7.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.8.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7.9.dart';
+import 'package:praktikum_flutter1/modul_7/modul_7_evaluasi.dart';
+import 'package:praktikum_flutter1/modul_7/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
